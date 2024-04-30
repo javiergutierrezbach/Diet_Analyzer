707e102f748db3695bcb605076dcdeb3aa19a08c
Javier Gutierrez Bach <javier.gutierrez.bach@gmail.com>
2024-04-30 18:16:40 -0400

    Added support for choosing quantities in the meal selector

--- mealselector/app.R
+++ mealselector/app.R
@@ -12,6 +12,17 @@ library(CanadianNutrient)
 library(DT)
 
 
+
+get_measurements <- function(item) {
+  measurements <- FoodNames |> 
+    filter(food_description == item) |> 
+    inner_join(ConversionFactor, by = 'food_id') |> 
+    inner_join(MeasureNames, by = 'measure_id') |> 
+    select(measure_description)
+  
+  return (measurements)
+}
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -21,6 +32,8 @@ ui <- fluidPage(
            selectInput("dropdown", label = "What did you eat today?",
                        choices = c("", unique(FoodNames$food_description))
                       ),
+           selectInput("measure_list", label = "How much of it did you eat?",
+                       choices = NULL),
            actionButton("add_button", "Add to your meal"),
            actionButton("remove_button", "Remove from your meal"),
            DTOutput("my_meal"),
@@ -30,22 +43,36 @@ ui <- fluidPage(
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
 
   selected_foods <- reactiveVal(c()) 
+  selected_measures <- reactiveVal(c()) 
+  
+  measurements <- NULL
+  
+  
+  observeEvent(input$dropdown, {
+    measurements <- get_measurements(input$dropdown)
+    updateSelectInput(session, "measure_list", choices = c("", measurements), selected = "")
+  })
   
   observeEvent(input$add_button, {
-    if(input$dropdown != ""){
+    if(input$dropdown != "" & input$measure_list != ""){
+      
       current_foods <- selected_foods()
       updated_foods <- c(current_foods, input$dropdown)
       selected_foods(updated_foods) 
+      
+      current_measures <- selected_measures()
+      updated_measures <- c(current_measures, input$measure_list)
+      selected_measures(updated_measures) 
     }
   })
   
   
   
   output$my_meal <- renderDT({
-    my_dataframe <- data.frame(Value = selected_foods())
+    my_dataframe <- data.frame(food_item = selected_foods(), measurement = selected_measures())
     datatable(my_dataframe, options = list(paging = FALSE))
   })
   
@@ -55,6 +82,10 @@ server <- function(input, output) {
       current_foods <- selected_foods()
       updated_foods <- current_foods[-input$my_meal_rows_selected]
       selected_foods(updated_foods) 
+      
+      current_measures <- selected_measures()
+      updated_measures <- current_measures[-input$my_meal_rows_selected]
+      selected_measures(updated_measures) 
     }
   })
   
